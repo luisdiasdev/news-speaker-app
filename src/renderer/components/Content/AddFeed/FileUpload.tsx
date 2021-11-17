a6c62b4dd05dc415f0bf220d0fee551c84f6c0f8
Luis Gustavo <luis.gustavo.dias@hotmail.com>
2021-11-17 10:24:04 -0300

    wip: add react-dropzone for file upload

--- src/renderer/components/Content/AddFeed/FileUpload.tsx
+++ src/renderer/components/Content/AddFeed/FileUpload.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { AiOutlineFolderOpen } from 'react-icons/ai'
 import styled from 'styled-components'
 
 import borderSvgUrl from '../../../../assets/svg/border.svg'
 
-const Background = styled.div`
+type BackgroundProps = {
+  isDragging: boolean
+}
+
+const Background = styled.div<BackgroundProps>`
   width: 100%;
   height: 100%;
-  background-color: #fff;
+  background-color: ${p => (p.isDragging ? '#CCC' : '#FFF')};
   opacity: 0.25;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `
 
 const FileUploadContainer = styled.div`
@@ -18,12 +28,30 @@ const FileUploadContainer = styled.div`
 `
 
 const FileUpload: React.FC = () => {
+  const onDrop = useCallback(acceptedFiles => {
+    // TODO: Handle file upload
+    console.log(acceptedFiles)
+  }, [])
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({
+    onDrop
+  })
+
   return (
-    <>
-      <FileUploadContainer>
-        <Background />
-      </FileUploadContainer>
-    </>
+    <FileUploadContainer
+      {...getRootProps({ isDragActive, isDragAccept, isDragReject })}
+    >
+      <Background isDragging={isDragActive || isDragAccept}>
+        <input {...getInputProps()} />
+        <AiOutlineFolderOpen fontSize='64px' />
+        <p>Drag here or click to browse your OPML file</p>
+      </Background>
+    </FileUploadContainer>
   )
 }
 
